73ce7dd01d704bdaf73a76ee9cec06c27092e1f8
KunaKamala <kunakamala123@gmail.com>
2026-08-08 07:54:20 +0530

    Fix asgi.py django setup order

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import rooms.routing
 from rooms.middleware import JWTAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http':      get_asgi_application(),
